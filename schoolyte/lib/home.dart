d1bbb5353aeece2ef849d950635a56d8cb0288dc
budionoaryawisanto <budionoarya.w@gmail.com>
2022-10-02 20:30:02 +0700

    Progres 10% home page

--- schoolyte/lib/home.dart
+++ schoolyte/lib/home.dart
@@ -25,14 +25,28 @@ class _HomePageState extends State<HomePage> {
     return new Scaffold(
       appBar: AppBar(
         elevation: 1,
-        iconTheme: IconThemeData(color: Colors.black),
+        iconTheme: IconThemeData(color: Color.fromARGB(255, 66, 65, 65)),
         backgroundColor: Colors.white,
         systemOverlayStyle: const SystemUiOverlayStyle(
-          systemNavigationBarDividerColor: Colors.black,
+          systemNavigationBarDividerColor: Color.fromRGBO(98, 103, 117, 1),
           statusBarColor: Colors.white,
           statusBarBrightness: Brightness.dark,
           statusBarIconBrightness: Brightness.dark,
         ),
+        actions: <Widget>[
+          Container(
+            margin: EdgeInsetsDirectional.only(end: 10),
+            child: TextButton(
+              onPressed: () {
+                Navigator.of(context).pushNamedAndRemoveUntil(
+                    '/landing', (Route<dynamic> route) => false);
+              },
+              child: Image.asset(
+                'assets/images/lonceng.png',
+              ),
+            ),
+          ),
+        ],
       ),
       drawer: Drawer(
         backgroundColor: Colors.white,
@@ -59,6 +73,73 @@ class _HomePageState extends State<HomePage> {
             ListTile(
               leading: Icon(Icons.settings),
               title: Text('Settings'),
+            ),
+          ],
+        ),
+      ),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment(0.0, 0.5),
+              child: Container(
+                width: 403,
+                height: 375,
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    width: 1,
+                    color: Colors.black,
+                  ),
+                ),
+                child: Stack(
+                  children: [
+                    Align(
+                      child: Container(
+                        margin: EdgeInsets.only(top: 10),
+                        alignment: Alignment(-1.0, -0.9),
+                        child: Text(
+                          'Berita Sekolah',
+                          style: TextStyle(
+                            fontFamily: 'Gilroy-ExtraBold',
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment(1.0, -0.9),
+                      child: TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pushNamedAndRemoveUntil(
+                              '/landing', (Route<dynamic> route) => false);
+                        },
+                        child: Container(
+                          width: 87,
+                          height: 18,
+                          decoration: BoxDecoration(
+                            color: Color.fromRGBO(180, 176, 255, 1),
+                            border: Border.all(
+                              width: 1,
+                              color: Color.fromRGBO(180, 176, 255, 1),
+                            ),
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: Center(
+                            child: Text(
+                              "Lihat Semua",
+                              style: TextStyle(
+                                fontFamily: 'Gilroy-ExtraBold',
+                                fontSize: 12,
+                                color: Color.fromRGBO(119, 115, 205, 1),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ),
           ],
         ),
