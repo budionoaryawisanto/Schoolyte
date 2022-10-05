d37243cd33658f3524b2c76d15c13072b6ba4b27
budionoaryawisanto <budionoarya.w@gmail.com>
2022-10-05 21:41:35 +0700

    Progres home sudah tinggal drawer

--- schoolyte/lib/home.dart
+++ schoolyte/lib/home.dart
@@ -1,9 +1,11 @@
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:schoolyte/main.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'dart:math';
+import 'package:dropdown_search/dropdown_search.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -101,7 +103,7 @@ class _HomePageState extends State<HomePage> {
           body: SingleChildScrollView(
             child: Container(
               width: 490,
-              height: 1184,
+              height: 1100,
               child: Stack(
                 children: [
                   Align(
@@ -174,10 +176,10 @@ class _HomePageState extends State<HomePage> {
                             '/landing', (Route<dynamic> route) => false);
                       },
                       child: Container(
-                        width: 462,
+                        width: 490,
                         height: 216,
                         decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(22),
+                          color: Color.fromRGBO(247, 247, 247, 1),
                         ),
                         child: Center(
                           child: new Image.asset(
@@ -189,16 +191,333 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                   Align(
-                    alignment: Alignment(0.0, 0.8),
+                    alignment: Alignment(0.0, -0.2),
+                    child: Container(
+                      width: 410,
+                      height: 190,
+                      child: Stack(
+                        children: [
+                          Align(
+                            alignment: Alignment(-0.9, -1.0),
+                            child: TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pushNamedAndRemoveUntil(
+                                    '/landing',
+                                    (Route<dynamic> route) => false);
+                              },
+                              child: Container(
+                                width: 56,
+                                height: 83,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                ),
+                                child: Stack(
+                                  children: [
+                                    Align(
+                                      alignment: Alignment(0.0, -1.0),
+                                      child: new Image.asset(
+                                        'assets/images/Frame 125.png',
+                                      ),
+                                    ),
+                                    Align(
+                                      alignment: Alignment(0.0, 0.8),
+                                      child: Text(
+                                        'Jadwal Kelas',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontFamily: 'Gilroy-Light',
+                                          fontSize: 11,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment(-0.3, -1.0),
+                            child: TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pushNamedAndRemoveUntil(
+                                    '/landing',
+                                    (Route<dynamic> route) => false);
+                              },
+                              child: Container(
+                                width: 56,
+                                height: 83,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                ),
+                                child: Stack(
+                                  children: [
+                                    Align(
+                                      alignment: Alignment(0.0, -1.0),
+                                      child: new Image.asset(
+                                        'assets/images/Frame 126.png',
+                                      ),
+                                    ),
+                                    Align(
+                                      alignment: Alignment(0.0, 0.7),
+                                      child: Text(
+                                        'Rapor',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontFamily: 'Gilroy-Light',
+                                          fontSize: 11,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment(0.3, -1.0),
+                            child: TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pushNamedAndRemoveUntil(
+                                    '/landing',
+                                    (Route<dynamic> route) => false);
+                              },
+                              child: Container(
+                                width: 56,
+                                height: 83,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                ),
+                                child: Stack(
+                                  children: [
+                                    Align(
+                                      alignment: Alignment(0.0, -1.0),
+                                      child: new Image.asset(
+                                        'assets/images/Frame 127.png',
+                                      ),
+                                    ),
+                                    Align(
+                                      alignment: Alignment(0.0, 0.5),
+                                      child: Text(
+                                        'Absensi',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontFamily: 'Gilroy-Light',
+                                          fontSize: 11,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment(0.9, -1.0),
+                            child: TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pushNamedAndRemoveUntil(
+                                    '/landing',
+                                    (Route<dynamic> route) => false);
+                              },
+                              child: Container(
+                                width: 56,
+                                height: 83,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                ),
+                                child: Stack(
+                                  children: [
+                                    Align(
+                                      alignment: Alignment(0.0, -1.0),
+                                      child: new Image.asset(
+                                        'assets/images/Frame 128.png',
+                                      ),
+                                    ),
+                                    Align(
+                                      alignment: Alignment(0.0, 0.5),
+                                      child: Text(
+                                        'Fasilitas',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontFamily: 'Gilroy-Light',
+                                          fontSize: 11,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment(-0.9, 1.0),
+                            child: TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pushNamedAndRemoveUntil(
+                                    '/landing',
+                                    (Route<dynamic> route) => false);
+                              },
+                              child: Container(
+                                width: 71,
+                                height: 72,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                ),
+                                child: Stack(
+                                  children: [
+                                    Align(
+                                      alignment: Alignment(0.0, -1.0),
+                                      child: new Image.asset(
+                                        'assets/images/Frame 129.png',
+                                      ),
+                                    ),
+                                    Align(
+                                      alignment: Alignment(0.0, 0.7),
+                                      child: Text(
+                                        'Perpustakaan',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontFamily: 'Gilroy-Light',
+                                          fontSize: 11,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment(-0.2, 1.0),
+                            child: TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pushNamedAndRemoveUntil(
+                                    '/landing',
+                                    (Route<dynamic> route) => false);
+                              },
+                              child: Container(
+                                width: 77,
+                                height: 84.5,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                ),
+                                child: Stack(
+                                  children: [
+                                    Align(
+                                      alignment: Alignment(0.0, -1.0),
+                                      child: new Image.asset(
+                                        'assets/images/Frame 130.png',
+                                      ),
+                                    ),
+                                    Align(
+                                      alignment: Alignment(0.0, 0.7),
+                                      child: Text(
+                                        'Ekstrakulikuler',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontFamily: 'Gilroy-Light',
+                                          fontSize: 11,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment(0.4, 0.9),
+                            child: TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pushNamedAndRemoveUntil(
+                                    '/landing',
+                                    (Route<dynamic> route) => false);
+                              },
+                              child: Container(
+                                width: 57,
+                                height: 69,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                ),
+                                child: Stack(
+                                  children: [
+                                    Align(
+                                      alignment: Alignment(0.0, -1.0),
+                                      child: new Image.asset(
+                                        'assets/images/Frame 131.png',
+                                      ),
+                                    ),
+                                    Align(
+                                      alignment: Alignment(0.0, 0.9),
+                                      child: Text(
+                                        'Kantin',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontFamily: 'Gilroy-Light',
+                                          fontSize: 11,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment(1.0, 1.0),
+                            child: TextButton(
+                              onPressed: () {
+                                Navigator.of(context).pushNamedAndRemoveUntil(
+                                    '/landing',
+                                    (Route<dynamic> route) => false);
+                              },
+                              child: Container(
+                                width: 85,
+                                height: 80,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                ),
+                                child: Stack(
+                                  children: [
+                                    Align(
+                                      alignment: Alignment(0.0, -1.0),
+                                      child: new Image.asset(
+                                        'assets/images/Frame 132.png',
+                                      ),
+                                    ),
+                                    Align(
+                                      alignment: Alignment(0.0, 0.8),
+                                      child: Text(
+                                        'Pembayaran SPP',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                          fontFamily: 'Gilroy-Light',
+                                          fontSize: 11,
+                                          color: Colors.black,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Align(
+                    alignment: Alignment(0.0, 0.7),
                     child: Container(
                       width: 403,
                       height: 375,
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          width: 1,
-                          color: Colors.black,
-                        ),
-                      ),
                       child: Stack(
                         children: [
                           Align(
